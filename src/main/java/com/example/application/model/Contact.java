fd7f5907212e839de6afc89e6ce357f004259fb3
Ujwal Ghale Pariyar <me.ujwal054@gmail.com>
2019-09-24 14:46:22 +0545

    Contact model updated

--- src/main/java/com/example/application/model/Contact.java
+++ src/main/java/com/example/application/model/Contact.java
@@ -1,4 +1,53 @@
 package com.example.application.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "contact")
 public class Contact {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "message_id",columnDefinition = "serial")
+    private int id;
+
+    @Column(name="email")
+    private  String email;
+
+    @Column(name="name")
+    private String name;
+
+    @Column(name = "message")
+    private String message;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
 }

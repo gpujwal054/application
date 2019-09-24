3d709149996147aecbd86a2772dc7a9dd0b757ad
Ujwal Ghale Pariyar <me.ujwal054@gmail.com>
2019-09-24 14:47:09 +0545

    Contact repository updated

--- src/main/java/com/example/application/repositories/ContactRepository.java
+++ src/main/java/com/example/application/repositories/ContactRepository.java
@@ -1,4 +1,8 @@
 package com.example.application.repositories;
 
-public interface ContactRepository {
+import com.example.application.model.Contact;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ContactRepository extends JpaRepository<Contact,Integer> {
+    Contact getMessageById(Integer id);
 }
